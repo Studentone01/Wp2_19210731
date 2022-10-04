d0078f98c4932375782c19768ed6d00a6f839be8
Studentone01 <studiedstudent01@gmail.com>
2022-10-04 08:13:43 +0700

    Adding .gitignore, .htaccess, application/, composer.json, index.php, license.txt, readme.rst, system/

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Ali Yusuf
+ Saya tingga di daerah Pamulang
+ olah raga yang saya sukai adalah
+ Berenang";
+ }
+}
